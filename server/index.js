228f5f05091af6d53769ab83eb9794b71012a22a
Nandini <nandiniverma202021@gmail.com>
2025-07-25 12:36:49 +0530

    Fix: Dynamic PORT for Render Deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.get('/', (_, res) => {
   res.send('AI vs HI Translator Backend is Running');
